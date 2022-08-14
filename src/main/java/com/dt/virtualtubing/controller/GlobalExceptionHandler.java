f8d120af4c7e405f299f44ad78862a5bbce251cf
Gustavo Correa <gustavo.correa@sap.com>
2022-08-14 16:19:37 -0300

    updated GlobalExceptionHandler

--- src/main/java/com/dt/virtualtubing/controller/GlobalExceptionHandler.java
+++ src/main/java/com/dt/virtualtubing/controller/GlobalExceptionHandler.java
@@ -1,16 +1,22 @@
 package com.dt.virtualtubing.controller;
 
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.dt.virtualtubing.exception.PdgNotFoundException;
 import com.dt.virtualtubing.exception.TubingNotFoundException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -28,6 +34,31 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(DateTimeParseException.class)
 	public ResponseEntity<List<String>> handleDateTimeParseException(DateTimeParseException e) {
 		return new ResponseEntity<>(toListMessages(e.getMessage()), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(InvalidFormatException.class)
+	protected ResponseEntity<List<String>> handleInvalidFormatException(InvalidFormatException e) {
+		return new ResponseEntity<>(toListMessages(e.getOriginalMessage()), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	protected ResponseEntity<List<String>> handleIllegalArgumentException(IllegalArgumentException e) {
+		return new ResponseEntity<>(toListMessages(e.getMessage()), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(JsonMappingException.class)
+	public ResponseEntity<List<String>> handleJsonMappingExceptionException(JsonMappingException e) {
+		return new ResponseEntity<>(toListMessages(e.getOriginalMessage()), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<List<String>> handleValidationException(MethodArgumentNotValidException e) {
+		BindingResult result = e.getBindingResult();
+		List<String> errorMessages = new ArrayList<>();
+		for (FieldError fieldError : result.getFieldErrors()) {
+			errorMessages.add(String.format("Field %s: %s", fieldError.getField(), fieldError.getDefaultMessage()));
+		}
+		return new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
 	}
 
 	private List<String> toListMessages(String message) {
